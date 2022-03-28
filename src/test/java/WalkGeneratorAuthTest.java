10ed5fed95c22070ed7c2518d96f84db2ac3b48f
mj davoudi <mjavaddavoud@gmail.com>
2022-03-28 14:15:40 +0430

    add some new test method

--- src/test/java/WalkGeneratorAuthTest.java
+++ src/test/java/WalkGeneratorAuthTest.java
@@ -2,20 +2,68 @@ import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 public class WalkGeneratorAuthTest {
+
     @Test
-    void take_time_test() {
+    void dont_moving_and_returning_to_starting_point() {
+        char[] walkDirection = {};
+        WalkGeneratorAuth walkGeneratorAuth = new WalkGeneratorAuth(walkDirection);
+        Assertions.assertTrue(walkGeneratorAuth.willReturnToStartingPoint());
+    }
+
+    @Test
+    void moving_and_dont_return_to_starting_point() {
+        char[] walkDirection = {'n', 'e'};
+        WalkGeneratorAuth walkGeneratorAuth = new WalkGeneratorAuth(walkDirection);
+        Assertions.assertFalse(walkGeneratorAuth.willReturnToStartingPoint());
+    }
+
+    @Test
+    void simple_walk_to_return_to_starting_point() {
         char[] walkDirection = {'n', 'w', 's', 'e'};
         WalkGeneratorAuth walkGeneratorAuth = new WalkGeneratorAuth(walkDirection);
-        int time = 4;
+        Assertions.assertTrue(walkGeneratorAuth.willReturnToStartingPoint());
+
+    }
+
+    @Test
+    void complex_walk_to_return_to_starting_point() {
+        char[] walkDirection = {'n','s','n','s','n','s','n','s','n','s'};
+        WalkGeneratorAuth walkGeneratorAuth = new WalkGeneratorAuth(walkDirection);
+        Assertions.assertTrue(walkGeneratorAuth.willReturnToStartingPoint());
+    }
+
+
+    @Test
+    void take_zero_time() {
+        char[] walkDirection = {};
+        WalkGeneratorAuth walkGeneratorAuth = new WalkGeneratorAuth(walkDirection);
+        int time = 0;
         Assertions.assertTrue(walkGeneratorAuth.takeTime(time));
     }
 
     @Test
-    void return_to_starting_point() {
-        char[] walkDirection = {'n','s','n','s','n','s','n','s','n','s', 's', 'n'};
+    void take_more_than_zero_time() {
+        char[] walkDirection = {'n','s','n','s','n'};
         WalkGeneratorAuth walkGeneratorAuth = new WalkGeneratorAuth(walkDirection);
-        Assertions.assertTrue(walkGeneratorAuth.willReturnToStartingPoint());
+        int time = 5;
+        Assertions.assertTrue(walkGeneratorAuth.takeTime(time));
+    }
 
+    @Test
+    void dont_move_complete_test() {
+        char[] walkDirection = {};
+        int time = 0;
+        WalkGeneratorAuth walkGeneratorAuth = new WalkGeneratorAuth(walkDirection);
+        Assertions.assertTrue(walkGeneratorAuth.willReturnToStartingPoint() && walkGeneratorAuth.takeTime(time));
+
+    }
+
+    @Test
+    void simple_move_complete_test() {
+        char[] walkDirection = {'n', 'w', 's', 'e'};
+        int time = 4;
+        WalkGeneratorAuth walkGeneratorAuth = new WalkGeneratorAuth(walkDirection);
+        Assertions.assertTrue(walkGeneratorAuth.willReturnToStartingPoint() && walkGeneratorAuth.takeTime(time));
     }
 
     @Test
@@ -24,6 +72,5 @@ public class WalkGeneratorAuthTest {
         int time = 10;
         WalkGeneratorAuth walkGeneratorAuth = new WalkGeneratorAuth(walkDirection);
         Assertions.assertTrue(walkGeneratorAuth.willReturnToStartingPoint() && walkGeneratorAuth.takeTime(time));
-
     }
 }
